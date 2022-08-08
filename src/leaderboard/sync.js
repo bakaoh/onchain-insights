650ca0f697c04cb2e3033d822b049649d8b32517
Bakaoh <batatsar@gmail.com>
2022-08-08 16:06:15 +0700

    Add leaderboard

--- src/leaderboard/sync.js
+++ src/leaderboard/sync.js
@@ -1,6 +1,5 @@
 const Crawler = require("../common/crawler");
 const { web3 } = require('../common/network').getConfig();
-const { pairModel } = require('./model');
 
 const SYNC_TOPIC = '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1';
 
