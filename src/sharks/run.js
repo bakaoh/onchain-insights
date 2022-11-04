da5088f5abc89868362a25588447650b72f22897
Bakaoh <batatsar@gmail.com>
2022-11-04 14:20:20 +0700

    Update sharks data

--- src/sharks/run.js
+++ src/sharks/run.js
@@ -11,31 +11,31 @@ async function run() {
     const storage = new Storage('20221104');
 
     // Crawl token+holder
-    const topTokens = await getTopTokens();
-    storage.writeTopTokens(topTokens)
-    for (let token of topTokens) {
-        console.log(`Crawl holders ${token.address}`);
-        const topHolders = await getTopHolders(token.address);
-        storage.writeTopHolders(token.address, topHolders);
-    }
+    // const topTokens = await getTopTokens();
+    // storage.writeTopTokens(topTokens)
+    // for (let token of topTokens) {
+    //     console.log(`Crawl holders ${token.address}`);
+    //     const topHolders = await getTopHolders(token.address);
+    //     storage.writeTopHolders(token.address, topHolders);
+    // }
 
     // Crawl top 50 holders' transfer
-    // const topTokens = await storage.loadTopTokens()
-    // for (let i = 0; i < 200; i++) {
-    //     const token = topTokens[i];
-    //     const topHolders = await storage.loadTopHolders(token.address);
-    //     let holderCount = 0;
-    //     console.log(`Crawl ${token.address}, ${new Date()}`);
-    //     for (let holder of topHolders) {
-    //         if (holderCount > 50) break;
-    //         if (holder.isContract == "true" || holder.name != holder.address) continue;
-    //         if (storage.hasTokenTransfers(holder.address)) continue;
-    //         console.log(`Crawl transfers ${holder.address}`);
-    //         const transfers = await getTokenTransfers(holder.address);
-    //         storage.writeTokenTransfers(holder.address, transfers);
-    //         holderCount++;
-    //     }
-    // }
+    const topTokens = await storage.loadTopTokens()
+    for (let i = 0; i < 200; i++) {
+        const token = topTokens[i];
+        const topHolders = await storage.loadTopHolders(token.address);
+        let holderCount = 0;
+        console.log(`Crawl ${token.address}, ${new Date()}`);
+        for (let holder of topHolders) {
+            if (holderCount > 50) break;
+            if (holder.isContract == "true" || holder.name != holder.address) continue;
+            if (storage.hasTokenTransfers(holder.address)) continue;
+            console.log(`Crawl transfers ${holder.address}`);
+            const transfers = await getTokenTransfers(holder.address);
+            storage.writeTokenTransfers(holder.address, transfers);
+            holderCount++;
+        }
+    }
 
     // const countTransfer = (transfers) => {
     //     const currentBN = 21994385;
